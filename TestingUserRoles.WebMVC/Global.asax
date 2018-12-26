e5115757c84ef3911f2dec96db05215dfe37b0a5
J Jones <jlssmmns93@gmail.com>
2018-12-26 06:50:17 -0500

    Add project files.

---
+++ TestingUserRoles.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestingUserRoles.WebMVC.MvcApplication" Language="C#" %>
---
+++ TestingUserRoles.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestingUserRoles.WebMVC.Startup))]
+namespace TestingUserRoles.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
